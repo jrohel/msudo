2ef6f35289eed7301563be4f0823c4b83d2ba97f
roh <roh@30e310cc-b365-5d4b-8166-626653d85fd0>
2013-10-29 07:59:04 +0000

    msudo - Execute a command as another user version 0.2. Added support for regexp in config file
    Example of config line, user "rohel" can start "/usr/sbin/arping some_parameters":
    rohel ^/usr/sbin/arping.*$
    
    git-svn-id: https://ais-dataserver:8443/svn/WinConfig/trunk/projects/msudo@2951 30e310cc-b365-5d4b-8166-626653d85fd0

--- msudo.c
+++ msudo.c
@@ -1,57 +1,82 @@
+/*
+msudo - Execute a command as another user.
+
+Jaroslav Rohel, 2013
+email: jaroslav.rohel@ais-brno.cz
+*/
+
+#define CFG_FILE_NAME "/etc/msudo.conf"
+//#define CFG_FILE_NAME "msudo.conf"
+
 #include <pwd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-
-//#define CFG_FILE_NAME "/etc/msudo.conf"
-#define CFG_FILE_NAME "msudo.conf"
+#include <regex.h>
 
 int main(int argc, const char *argv[])
 {
 	FILE *cfgFile;
-	const char *wantPrgName;
-	char username[1024];
-	char prgname[1024];
 	uid_t my_uid;
 	struct passwd *pw;
 
 	if (argc!=2 || argv[1]=='\0')
 	{
-		fprintf(stdout, "msudo version 0.1     Jaroslav Rohel, 2013\n", argv[0]);
+		fprintf(stdout, "msudo version 0.2     Jaroslav Rohel, 2013\n\n", argv[0]);
 		fprintf(stdout, "Usage:\n");
-		fprintf(stdout, "%s pathToPrgForExec\n\n", argv[0]);
+		fprintf(stdout, "%s pathToPrgForExec_with_arguments\n\n", argv[0]);
+		fprintf(stdout, "Example:\n");
+		fprintf(stdout, "%s \"/usr/sbin/arping -i eth0 192.168.10.100\"\n\n", argv[0]);
 		return -1;
 	}
 
 	// get my real name
 	my_uid = getuid();
 	pw = getpwuid(my_uid);
-//	printf("%s\n", pw->pw_name);
 
-	wantPrgName = argv[1];
 	cfgFile = fopen(CFG_FILE_NAME, "r");
 	if (cfgFile)
 	{
+		const char *wantPrg = argv[1];
+
+		char username[1024];
+		char pattern[1024];
+//		char *pattern = "a[a-c]u";
 		int found = 0;
-		while (fscanf(cfgFile, "%1023s%*10000[ \t]%1023s%*10000[ \t\n\r]", username, prgname) >= 2)
+
+		while (fscanf(cfgFile, "%1023s%*10000[ \t]%1023[^\n\r]%*10000[\n\r]", username, pattern) >= 2)
 		{
 //			printf("UserName: %s\n", username);
 //			printf("PrgName: %s\n", prgname);
-			if (strcmp(username, pw->pw_name)==0 && strcmp(wantPrgName, prgname)==0)
+			if (strcmp(username, pw->pw_name) == 0)
 			{
-				found = 1;
-				break;
+				regex_t preg;
+				int rc;
+
+				rc = regcomp(&preg, pattern, 0);
+				if (rc)
+				{
+					fprintf(stderr, "regcomp() failed, returning nonzero (%d)\n", rc);
+					break;
+				}
+				rc = regexec(&preg, wantPrg, 0, NULL, 0);
+				regfree(&preg);
+				if (!rc)
+				{
+					found = 1;
+					break;
+				}
 			}
 		}
 		fclose(cfgFile);
 		if (found)
 		{
 			printf("Starting\n");
-			system(wantPrgName);
+			system(wantPrg);
 		}
-		else fprintf(stdout, "User \"%s\" is not allowed to start program %s\n", pw->pw_name, wantPrgName);
+		else fprintf(stdout, "User \"%s\" is not allowed to start \"%s\"\n", pw->pw_name, wantPrg);
 	}
 	else fprintf(stderr, "Can't open config file %s\n", CFG_FILE_NAME);
 	return 0;
